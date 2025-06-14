06424cefc6c57c20bf1c0bc2a123666732becabc
BharathSunkarKumar <155315751+BharathSunkaraKumar@users.noreply.github.com>
2025-06-14 19:46:08 +0530

    links changed

--- app/components/ProductCollections.jsx
+++ app/components/ProductCollections.jsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react"
 export default function ProductCollections() {
     const [collections, setCollections] = useState('');
     const fetchCollections = async() => {
-        const response = await fetch('http://localhost:3000/api/admin/add-product');
+        const response = await fetch('https://resort-booking-jv15ck5g2-bharaths-projects-aa325104.vercel.app/api/admin/add-product');
         const newData = await response.json();
         // console.log(data)
         setCollections(newData.data)
